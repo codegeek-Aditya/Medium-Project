b65b7f31a8475d768c6a16abb8c0bf24734a61a5
codegeek-Aditya <heis.adityasinha@gmail.com>
2024-07-21 16:04:14 +0530

    add styling to skeleton.tsx

--- mediumproject/frontend/src/components/Skeleton.tsx
+++ mediumproject/frontend/src/components/Skeleton.tsx
@@ -3,57 +3,57 @@ export const Skeleton = () => {
 
         <div role="status" className="p-4 space-y-4 m-4 max-w-mdrounded shadow animate-pulse  md:p-6 ">
             <div className="flex items-center justify-between">
-                <div>
-                    <div className="h-3 bg-gray-500 rounded-full dark:bg-gray-600 w-40 mb-3"></div>
-                    <div className="w-60 h-3 bg-gray-200 rounded-full"></div>
+                <div className="w-full">
+                    <div className="h-3 bg-gray-500 rounded-full dark:bg-gray-600 mb-3 w-full"></div>
+                    <div className="w-96 h-3 bg-gray-300 rounded-full"></div>
                 </div>
             </div>
             <div className="flex items-center justify-between pt-4">
-                <div>
-                    <div className="h-3 bg-gray-500 rounded-full w-40 mb-3"></div>
-                    <div className="w-60 h-3 bg-gray-200 rounded-full"></div>
+                <div className="w-full">
+                    <div className="h-3 bg-gray-500 rounded-full w-full mb-3"></div>
+                    <div className="w-96 h-3 bg-gray-300 rounded-full"></div>
                 </div>
             </div>
             <div className="flex items-center justify-between pt-4">
-                <div>
-                    <div className="h-3 bg-gray-500 rounded-full w-40 mb-3"></div>
-                    <div className="w-60 h-3 bg-gray-200 rounded-full"></div>
+                <div className="w-screen">
+                    <div className="h-3 bg-gray-500 rounded-full w-full mb-3"></div>
+                    <div className="w-96 h-3 bg-gray-300 rounded-full"></div>
                 </div>
             </div>
             <div className="flex items-center justify-between pt-4">
-                <div>
-                    <div className="h-3 bg-gray-500 rounded-full  w-40 mb-3"></div>
-                    <div className="w-60 h-3 bg-gray-200 rounded-full"></div>
+                <div className="w-screen">
+                    <div className="h-3 bg-gray-500 rounded-full  w-full mb-3"></div>
+                    <div className="w-96 h-3 bg-gray-300 rounded-full"></div>
                 </div>
             </div>
             <div className="flex items-center justify-between pt-4">
-                <div>
-                    <div className="h-3 bg-gray-500 rounded-full  w-40 mb-3"></div>
-                    <div className="w-60 h-3 bg-gray-200 rounded-full"></div>
+                <div className="w-screen">
+                    <div className="h-3 bg-gray-500 rounded-full  w-full mb-3"></div>
+                    <div className="w-96 h-3 bg-gray-300 rounded-full"></div>
                 </div>
             </div>
             <div className="flex items-center justify-between pt-4">
-                <div>
-                    <div className="h-3 bg-gray-500 rounded-full  w-40 mb-3"></div>
-                    <div className="w-60 h-3 bg-gray-200 rounded-full"></div>
+                <div className="w-screen">
+                    <div className="h-3 bg-gray-500 rounded-full  w-full mb-3"></div>
+                    <div className="w-96 h-3 bg-gray-300 rounded-full"></div>
                 </div>
             </div>
             <div className="flex items-center justify-between pt-4">
-                <div>
-                    <div className="h-3 bg-gray-500 rounded-full  w-40 mb-3"></div>
-                    <div className="w-60 h-3 bg-gray-200 rounded-full"></div>
+                <div className="w-screen">
+                    <div className="h-3 bg-gray-500 rounded-full w-full mb-3"></div>
+                    <div className="w-96 h-3 bg-gray-300 rounded-full"></div>
                 </div>
             </div>
             <div className="flex items-center justify-between pt-4">
-                <div>
-                    <div className="h-3 bg-gray-500 rounded-full w-40 mb-3"></div>
-                    <div className="w-60 h-3 bg-gray-200 rounded-full"></div>
+                <div className="w-screen">
+                    <div className="h-3 bg-gray-500 rounded-full w-full mb-3"></div>
+                    <div className="w-96 h-3 bg-gray-300 rounded-full"></div>
                 </div>
             </div>
             <div className="flex items-center justify-between pt-4">
-                <div>
-                    <div className="h-3 bg-gray-500 rounded-full w-40 mb-3"></div>
-                    <div className="w-60 h-3 bg-gray-200 rounded-full"></div>
+                <div className="w-screen">
+                    <div className="h-3 bg-gray-500 rounded-full w-full mb-3"></div>
+                    <div className="w-96 h-3 bg-gray-300 rounded-full"></div>
                 </div>
             </div>
         </div>
